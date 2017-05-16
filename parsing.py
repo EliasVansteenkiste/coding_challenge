a1eb818c7b1e1e3bce240405dac67a23873d509e
EliasVansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-05-16 14:38:51 +0200

    added convenience functions to list all available i-contours

--- parsing.py
+++ parsing.py
@@ -2,9 +2,12 @@
 
 import dicom
 from dicom.errors import InvalidDicomError
-
 import numpy as np
 from PIL import Image, ImageDraw
+import os
+
+import app
+import pathfinder
 
 
 def parse_contour_file(filename):
@@ -42,3 +45,34 @@ def poly_to_mask(polygon, width, height):
     ImageDraw.Draw(img).polygon(xy=polygon, outline=0, fill=1)
     mask = np.array(img).astype(bool)
     return mask
+
+def get_list_available_icontours():
+    icountours = []
+    pid2oid = app.read_pid2oid(pathfinder.LINK_PATH)
+    for key,value in pid2oid.iteritems():
+        path = pathfinder.CONTOURS_PATH+'/'+value+'/i-contours'
+        for filename in os.listdir(path):
+            icountours.append(path + '/' + filename)
+    return icountours
+
+def get_slice_id(icontour_path):
+    filename = icontour_path.split('/')[-1]
+    slice_id = int(filename.split('-')[2])
+    return slice_id
+
+def get_contour_id(icontour_path):
+    contour_id = icontour_path.split('/')[-3]
+    return contour_id
+
+def get_dicom_id(icontour_path, oid2pid = app.read_oid2pid(pathfinder.LINK_PATH)):
+    contour_id = icontour_path.split('/')[-3]
+    dicom_id = oid2pid[contour_id]
+    return dicom_id
+
+
+if __name__ == "__main__":
+    for idx, icp in enumerate(get_list_available_icontours()):
+        if idx == 100:
+            break
+        img, pixel_spacing = app.read_dicom_slice(get_dicom_id(icp), get_slice_id(icp))
+        print img.shape, pixel_spacing
