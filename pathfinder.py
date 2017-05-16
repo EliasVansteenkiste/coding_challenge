7dcbaf126c2c2ef695c9ead70ce9a530c693c4ee
Elias Vansteenkiste <Elias.Vansteenkiste@ugent.be>
2017-05-16 11:11:21 +0200

    some infrastructure work to get paths and easily accessible

---
+++ pathfinder.py
@@ -0,0 +1,18 @@
+import json
+import utils
+import os
+
+
+
+with open('SETTINGS.json') as data_file:
+    paths = json.load(data_file)
+
+CONTOURS_PATH = paths["CONTOURS_PATH"]
+utils.check_data_paths(CONTOURS_PATH)
+
+DICOMS_PATH = paths["DICOMS_PATH"]
+utils.check_data_paths(DICOMS_PATH)
+
+LINK_PATH = paths["LINK_PATH"]
+if not os.path.isfile(LINK_PATH):
+    raise ValueError('Could not find a link CSV file')
